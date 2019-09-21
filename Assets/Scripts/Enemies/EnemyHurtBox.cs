b9a1ba59505d283dddf9ccc99feee81beb895b52
pedroazvm <pedro.azvm@gmail.com>
2019-09-21 12:31:25 -0300

    Dano e texto

---
+++ Assets/Scripts/Enemies/EnemyHurtBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHurtBox : MonoBehaviour
+{
+    public LayerMask playerAttackLayer;
+
+    private BaseEnemy baseEnemy;
+
+    private void Awake()
+    {
+        baseEnemy = GetComponentInParent<BaseEnemy>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == playerAttackLayer) {
+
+            baseEnemy.TakeDamage(1);
+        }
+    }
+}
